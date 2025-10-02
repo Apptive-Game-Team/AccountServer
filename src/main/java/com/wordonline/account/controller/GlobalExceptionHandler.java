98e300323810a4fbf4ad49e01ebd584444be812d
Yunseong <dev.yunseong@gmail.com>
2025-10-02 16:51:52 +0900

    Enhance GlobalExceptionHandler with additional exception handling for IllegalArgumentException, AuthorizationDeniedException, and validation errors

--- src/main/java/com/wordonline/account/controller/GlobalExceptionHandler.java
+++ src/main/java/com/wordonline/account/controller/GlobalExceptionHandler.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,5 +24,32 @@ public class GlobalExceptionHandler {
         String message = String.join(" | ", ex.getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage
         ).toList());
         return Mono.just(ResponseEntity.badRequest().body(message));
+    }
+
+    private final static String INVALID_REQUEST_MESSAGE = "요청이 잘못됐습니다.";
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.trace(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<String> handleAuthorizationDeniedException(AuthorizationDeniedException e) {
+        log.trace(e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("Unauthorized");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
+        log.trace(e.getMessage());
+        return ResponseEntity.badRequest().body(INVALID_REQUEST_MESSAGE);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void handleException(Exception e) {
+        log.error("[ERROR] {}", e.getMessage(), e);
     }
 }
