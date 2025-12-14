e0ea6cf3971fc1d4ed72d1754d9728330baaa7bc
Yunseong <dev.yunseong@gmail.com>
2025-12-14 11:12:28 +0900

    Add R2DBC configuration for account and game databases

---
+++ src/main/java/com/wordonline/account/config/database/AccountDbConfig.java
@@ -0,0 +1,44 @@
+package com.wordonline.account.config.database;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+
+@Configuration
+@EnableR2dbcRepositories(
+    basePackages = "com.wordonline.account.repository",
+    entityOperationsRef = "accountEntityTemplate"
+)
+public class AccountDbConfig {
+
+    @Value("${spring.r2dbc.account.url}")
+    private String url;
+    @Value("${spring.r2dbc.account.username}")
+    private String username;
+    @Value("${spring.r2dbc.account.password}")
+    private String password;
+
+    @Bean(name = "accountConnectionFactory")
+    public ConnectionFactory accountConnectionFactory() {
+        ConnectionFactoryOptions baseOptions = ConnectionFactoryOptions.parse(url);
+
+        ConnectionFactoryOptions options = baseOptions.mutate()
+                .option(ConnectionFactoryOptions.USER, username)
+                .option(ConnectionFactoryOptions.PASSWORD, password)
+                .build();
+
+        return ConnectionFactories.get(options);
+    }
+
+    @Bean(name = "accountEntityTemplate")
+    public R2dbcEntityTemplate accountEntityTemplate(@Qualifier("accountConnectionFactory") ConnectionFactory connectionFactory) {
+        return new R2dbcEntityTemplate(connectionFactory);
+    }
+}
